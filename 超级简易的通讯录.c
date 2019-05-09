2b01165a9b27ff8fb7cd085b4c8dd88d5c9a2b95
xiaotianlu <4099366@qq.com>
2019-05-09 15:43:00 +0800

    Update 超级简易的通讯录.c

--- 超级简易的通讯录.c
+++ 超级简易的通讯录.c
@@ -1,109 +1,146 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void save(char *area,int n); 
-void show(char *area,int n); 
+void save(char* area, int n);
+void show(char* area, int n);
 struct s_list
 {
 	char name[20];
 	char tel[15];
-	char adr[50]; 
-}list[200];
+	char adr[50];
+}list[200] = { {{"0"},{"0"},{"0"} } };
+
+char* mystrcpy(char *dest ,char* src) 
+{
+	int i = 0;
+	while (src[i] != '\0')
+	{
+		//printf("%c\n", src[i]);
+		i++;
+	}
+	int size = i * sizeof(char) + 1;
+	dest = (char*)malloc(size);
+	if (dest == NULL)
+		return NULL;
+	strcpy(dest, src, i);
+	dest[size-1] = '\0';
+	printf("%s\n", dest);
+	return dest;
+}
 int main()
 {
-	char *area,areas[30];
-	int n; 
-	FILE *f;
-	if(fopen("D:\\list_xl","r")==NULL)
+	char* area = NULL;
+	char  areas[30] = {0};
+	int n;
+	FILE* f;
+	if (fopen("D:\\list_xl", "r") == NULL)
 	{
-		printf("»¶Ó­µÚÒ»´ÎÊ¹ÓÃ±¾Í¨Ñ¶Â¼\nÇëÉèÖÃÍ¨Ñ¶Â¼±£´æµÄ¾ø¶ÔµØÖ·\n");
-		scanf("%s",&areas);
-		f=fopen("D:\\list_xl","w");
-		fwrite(areas,strlen(areas),1,f); 
-		fclose(f); 
-	} 
-	else
-	{
+		printf("æ¬¢è¿ç¬¬ä¸€æ¬¡ä½¿ç”¨æœ¬é€šè®¯å½•\nè¯·è®¾ç½®é€šè®¯å½•ä¿å­˜çš„ç»å¯¹åœ°å€\n");
+		scanf("%s", &areas);
+		printf("æ–‡ä»¶å%s\n", areas);
+
+		area = mystrcpy(area, areas);
+		f = fopen("D:\\list_xl", "w");
+
+		fwrite(area, strlen(area), 1, f);
+		fclose(f);
 		
-		f=fopen("D:\\list_xl","r");
-		fseek( f , 0 , SEEK_END );
-        int file_size;
-        file_size = ftell( f);
-        fseek( f, 0 , SEEK_SET);
-        area=  (char *)malloc( file_size * sizeof( char ) );
-		fread(area,file_size,1,f); 
-		fclose(f); 
-		printf("ÒÑÔØÈëµÚÒ»´Î´´½¨µÄÍ¨Ñ¶Â¼\nĞèÒªÖØĞÂÉèÖÃÍ¨Ñ¶Â¼É¾³ıdÅÌÄ¿Â¼ÏÂlist_xlÎÄ¼ş¼´¿É£¡\n"); 
-	} 
-label:
-	printf("----¿Ì¹ÇÃúĞÄ¼òÒ×Í¨Ñ¶Â¼xl----\n1-´æ´¢\n2-ÏÔÊ¾\n");
-	int choice;
-	scanf("%d",&choice);
-	if(choice==1)
-	{
-		printf("\nÊäÈë´æ´¢ÁªÏµÈËĞÅÏ¢µÄÊıÁ¿\n");
-	scanf("%d",&n);
-	save(area,n);
-	goto label;
 	}
-	else if(choice==2) 
-	{
-		printf("\nÊäÈëÏÔÊ¾ÁªÏµÈËĞÅÏ¢µÄÊıÁ¿\n");
-	scanf("%d",&n);
-	show(area,n);
-	goto label;}
 	else
-	{printf("\n¹¦ÄÜÔİÎ´¿ª·¢\n");
-	goto label;}
-	return 0;	 
-} 
-void save(char *area,int n)
+	{
+
+		f = fopen("D:\\list_xl", "r");
+		fseek(f, 0, SEEK_END);
+		int file_size;
+		file_size = ftell(f);
+		printf("æ–‡ä»¶åå­—èŠ‚æ•°%d\n",file_size);
+		fseek(f, 0, SEEK_SET);
+		area = (char*)malloc(file_size * sizeof(char));
+		fread(area, file_size, 1, f);
+		printf("æ–‡ä»¶å%s\n", area);
+		fclose(f);
+		printf("å·²è½½å…¥ç¬¬ä¸€æ¬¡åˆ›å»ºçš„é€šè®¯å½•\néœ€è¦é‡æ–°è®¾ç½®é€šè®¯å½•åˆ é™¤dç›˜ç›®å½•ä¸‹list_xlæ–‡ä»¶å³å¯ï¼\n");
+	}
+label:
+	printf("----åˆ»éª¨é“­å¿ƒç®€æ˜“é€šè®¯å½•xl----\n1-å­˜å‚¨\n2-æ˜¾ç¤º\n3-é€€å‡º\n");
+	int choice;
+	scanf("%d", &choice);
+	if (choice == 1)
+	{
+		printf("\nè¾“å…¥å­˜å‚¨è”ç³»äººä¿¡æ¯çš„æ•°é‡\n");
+		scanf("%d", &n);
+		save(area, n);
+		goto label;
+	}
+	else if (choice == 2)
+	{
+		printf("\nè¾“å…¥æ˜¾ç¤ºè”ç³»äººä¿¡æ¯çš„æ•°é‡\n");
+		scanf("%d", &n);
+		show(area, n);
+		goto label;
+	}
+	else if (choice == 3)
+	{
+		exit(0);
+	}
+	else
+	{
+		printf("\nåŠŸèƒ½æš‚æœªå¼€å‘\n");
+		goto label;
+	}
+	return 0;
+}
+void save(char* area, int n)
 {
-	int i,p;
-	FILE *f; 
-	if((f=fopen(area,"a"))==NULL)
+	int i, p;
+	FILE* f;
+	printf("æ–‡ä»¶å%s\n", area);
+	if ((f = fopen(area, "a")) == NULL)
 	{
-		if((f=fopen(area,"w"))==NULL)
-		{ 
-		printf("\nÎÄ¼ş´´½¨Ê§°Ü£¡\n");
-		exit(0);} 
-	} 
-	printf("--ÇëÊäÈëÁªÏµÈËĞÕÃû£¬µç»°£¬µØÖ·--"); 
-	for(i=0;i<n;i++)
-	{
-		printf("\nµÚ%d¸öÁªÏµÈË\n",i+1);
-		scanf("%s%s%s",&list[i].name,&list[i].tel,&list[i].adr); 
-		p=fwrite(&list[i],sizeof(struct s_list),1,f);
-		if(p!=1)
+		if ((f = fopen(area, "w")) == NULL)
 		{
-			printf("ÎÄ¼şĞ´ÈëÊ§°Ü!");
-			exit(0); 
+			printf("\næ–‡ä»¶åˆ›å»ºå¤±è´¥ï¼\n");
+			exit(0);
+		}
+	}
+	printf("--è¯·è¾“å…¥è”ç³»äººå§“å,ç”µè¯,åœ°å€--");
+
+	for (i = 0; i < n; i++)
+	{
+		printf("\nç¬¬%dä¸ªè”ç³»äºº\n", i + 1);
+		scanf("%s%s%s", &list[i].name, &list[i].tel, &list[i].adr);
+		p = fwrite(&list[i], sizeof(struct s_list ), 1, f);
+		printf("å†™å…¥çŠ¶æ€%d",p);
+		if (p != 1)
+		{
+			printf("æ–‡ä»¶å†™å…¥å¤±è´¥!");
+			exit(0);
 		}
 		else
 		{
-			printf("ÎÄ¼şĞ´Èë³É¹¦£¡"); 
-		} 
-		 
+
+			printf("æ–‡ä»¶å†™å…¥æˆåŠŸï¼");
+		}
+
 	}
-	fclose(f); 
-	 
-} 
-void show(char *area,int n)
-{
-	FILE *f; 
-	int i; 
-	if((f=fopen(area,"r"))==NULL)
-	{
-		printf("\nÎÄ¼ş¶ÁÈ¡Ê§°Ü£¡");
-		exit(0); 
-	} 
-	
-	for (i=0;i<n;i++)
-	{ 
-	fread(&list[i],sizeof(struct s_list),1,f);
-	printf("\nĞÕÃû£º%s\nµç»°£º%s\nµØÖ·£º%s\n-------\n",list[i].name,list[i].tel,list[i].adr);
-	}
-	fclose(f); 
-	
+	fclose(f);
+
 }
- 
+void show(char* area, int n)
+{
+	FILE* f;
+	int i;
+	if ((f = fopen(area, "r")) == NULL)
+	{
+		printf("\næ–‡ä»¶è¯»å–å¤±è´¥ï¼");
+		exit(0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		fread(&list[i], sizeof(struct s_list), 1, f);
+		printf("\nå§“åï¼š%s\nç”µè¯ï¼š%s\nåœ°å€ï¼š%s\n-------\n", list[i].name, list[i].tel, list[i].adr);
+	}
+	fclose(f);
+
+}
